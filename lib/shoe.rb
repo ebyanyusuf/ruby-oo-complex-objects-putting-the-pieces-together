c28234cc7e0435da012b4428e8abaaad0702323b
ebyanyusuf <ebyan.yusuf@gmail.com>
2020-11-23 09:58:41 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,9 +8,9 @@ class Shoe
         @brand = brand
     end
 
-    def cobble
+    def cobble(condition = "new")
         puts "Your shoe is as good as new!"
-        @condition = "new"
+       
     end
 
     
